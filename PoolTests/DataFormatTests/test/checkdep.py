a4ae12dfb45be05bc365a25e86cd08d9b3767954
innocent <>
2006-12-04 09:50:37 +0000

    works

--- PoolTests/DataFormatTests/test/checkdep.py
+++ PoolTests/DataFormatTests/test/checkdep.py
@@ -1,8 +1,10 @@
 # for the time being only dataformat
 #
-import string
+import string, os, sys
+#
 class BuildFile :
     def __init__(self, filen) :
+        # print filen
         self.use=[]
         f = open(filen)
         lines = string.join(f.readlines())
@@ -18,7 +20,9 @@ class BuildFile :
             if (j<i) : break
             # print lines[i:j+1]
             self.use.append(lines[i+1:j].strip())
-
+    def depend(self,pack):
+        return pack in self.use            
+        
 class Headers :
     def __init__(self, filen, lpk) :
         self.localpack =lpk
@@ -45,29 +49,42 @@ class CheckDep :
      def __init__(self, src) :
          self.src=src
          self.dirs = ["DataFormats","SimDataFormats"]
-
+         self.report = ''
+         
      def check(self) :
          for bdir in self.dirs:
              topdir = self.src+'/'+bdir
              # print topdir
              for dir in os.listdir(topdir):
-                 self.iterate(bdir+'/'+dir)
+                 self.report += self.iterate(bdir+'/'+dir)
 
-     def iterate(self,sdir) :
+     def iterate(self,pack) :
          report=''
-         buildfile = sdir+'/BuldFile'
+         buildfile = pack+'/BuildFile'
          try:
-             bfile =BuildFile(self.src+'/'+Buildfife)
-             
+             bfile =BuildFile(self.src+'/'+buildfile)
          except:
-             report += "Package "+sdir+" does not have a BuildFile"
-
+             report += "Package "+pack+" does not have a BuildFile\n"
+         else:
+             interface = self.src+'/'+pack+'/interface'
+             for hname in os.listdir(interface):
+                 # print hname
+                 if (hname.find(".h")<1): continue
+                 headers = Headers(interface+'/'+hname,pack)
+                 for pk in headers.packages :
+                     if (not bfile.depend(pk)) :
+                         report += 'Missing use of ' + pk \
+                                   + ' in ' + pack + ' BuildFile\n'
+         return report
 
 def main():
     args = sys.argv[1:]
     if len(args) != 1:
         print 'usage: python checkdep.py $CMSSW_BASE/src'
         sys.exit(-1)
-
+    c = CheckDep(args[0])
+    c.check()
+    print c.report
+    
 if __name__ == '__main__':
     main()
