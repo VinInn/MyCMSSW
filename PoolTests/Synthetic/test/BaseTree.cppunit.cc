9e190b24e9c185a5f5ffa411667835d4f7788014
innocent <>
2007-01-12 16:31:06 +0000

    one more test

---
+++ PoolTests/Synthetic/test/BaseTree.cppunit.cc
@@ -0,0 +1,97 @@
+#include "PoolTests/Synthetic/interface/BaseTree.h"
+
+
+#include <cppunit/extensions/HelperMacros.h>
+
+
+namespace {
+
+  struct Stub : public synthetic::BaseBranch {
+    Stub() : m_globalCount(0){}
+    virtual  void write(int globalCount) {
+      m_globalCount=globalCount;
+    }
+
+    int m_globalCount;
+
+  };
+
+}
+
+class testBaseTree : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(testBaseTree);
+  CPPUNIT_TEST(check_data);
+  CPPUNIT_TEST(check_constr);
+  CPPUNIT_TEST(check_add);
+  CPPUNIT_TEST(check_find);
+  CPPUNIT_TEST(check_write);
+  CPPUNIT_TEST_SUITE_END();
+public:
+  void setUp();
+  void tearDown() {}
+  void check_data();
+  void check_constr();
+  void check_add();
+  void check_write();
+
+  Stub * add3Branches(BaseTree& bt, bool verify=false);
+
+  std::string fname;
+  std::string tname;
+  
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(testBaseTree);
+
+void testBaseBranch::setUp() {
+  fname = "fname";
+  tname = "tname";
+
+}
+
+void testBaseBranch::check_data() {
+  synthetic::BaseTree::Data d(fname,tname,0);
+  CPPUNIT_ASSERT (d.fname==fname);
+  CPPUNIT_ASSERT (d.tname==tname);
+  CPPUNIT_ASSERT (d.svc==0);
+}
+
+void testBaseBranch::check_constr() {
+  synthetic::BaseTree bt(0,fname,tname);
+  synthetic::BaseTree::Data const & d = bt.data();
+  CPPUNIT_ASSERT (d.fname==fname);
+  CPPUNIT_ASSERT (d.tname==tname);
+  CPPUNIT_ASSERT (d.svc==0);
+}
+
+void testBaseBranch::check_add() {
+  synthetic::BaseTree bt(0,fname,tname);
+  add3Branches(bt,true);
+}
+
+void testBaseBranch::check_write() {
+  synthetic::BaseTree bt(0,fname,tname);
+  Stub * bs = add3Branches(bt,false);
+
+  bt.write();
+  bt.write();
+  bt.write();
+
+  CPPUNIT_ASSERT (bs->m_globalCount==3);
+
+} 
+
+Stub * add3Branches(BaseTree& bt, bool verify) {
+  Stub * bs = new Stub();
+  CPPUNIT_ASSERT (bt.add("1", bs));
+  CPPUNIT_ASSERT (bt.add("2", new Stub()));
+  CPPUNIT_ASSERT (bt.add("3",  new Stub()));
+
+  if (verify) {
+    CPPUNIT_ASSERT (!bt.add("1", bs));
+    
+    // this is not protected....
+    CPPUNIT_ASSERT (bt.add("99", bs));
+  }
+  return bs;
+}
