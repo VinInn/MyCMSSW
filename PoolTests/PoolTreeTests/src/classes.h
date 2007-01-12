24383e3996a11a9392311eb59c9543372d61a5f1
innocent <>
2007-01-12 09:01:46 +0000

    added dict

--- PoolTests/PoolTreeTests/src/classes.h
+++ PoolTests/PoolTreeTests/src/classes.h
@@ -1,10 +1,11 @@
 #include "PoolTests/PoolTreeTests/interface/MyData.h"
 #include <vector>
+#include <string>
 
 namespace {
   namespace {
 
-    std::vector<int> dummy;
-
+    std::vector<int> dummy1;
+    std::vector<std::basic_string<char> > dummy2;
   }
 }
