f93fb4e74d3273ef4451394f432b0374fd4a9cd4
innocent <>
2007-01-19 14:45:00 +0000

    test

--- PerfTools/Service/test/ServiceFactory.cppunit.cc
+++ PerfTools/Service/test/ServiceFactory.cppunit.cc
@@ -1,60 +1,69 @@
-#include "PerfTools/Reporter/interface/Sampler.h"
+#include "PerfTools/Service/interface/ServiceFactory.h"
+#include<memory>
 
 #include <cppunit/extensions/HelperMacros.h>
 
 
 // FIXME
 // check by features....
-class TestSampler : public CppUnit::TestFixture {
-  CPPUNIT_TEST_SUITE(TestSampler);
-  CPPUNIT_TEST(check_Sampler);
+class TestServiceFactory : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(TestServiceFactory);
+  CPPUNIT_TEST(check_Maker);
+  CPPUNIT_TEST(check_getAny);
+  CPPUNIT_TEST(check_getService);
+  CPPUNIT_TEST(check_NameError);
+  CPPUNIT_TEST(check_TypeError);
   CPPUNIT_TEST_SUITE_END();
 public:
   void setUp(){}
   void tearDown() {}
-  void check_Sampler();
+  void check_Maker();
+  void check_getAny();
+  void check_getService();
+  void check_NameError();
+  void check_TypeError();
 
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(TestSampler);
+CPPUNIT_TEST_SUITE_REGISTRATION(TestServiceFactory);
 
+namespace perftools{
 
+  namespace serviceTest {
+    struct Dummy1{};
+    struct Dummy2{};
 
-namespace {
-
-  int more() {
-    static int a=0;
-    return a++;
   }
   
-  int last=0;
-  
-  void tell(int i){
-    last = i;
-  }
+
 }
 
-void  TestSampler::check_Sampler() {
+
+void TestServiceFactory::check_Maker() {
+  typedef perftools::serviceTest::Dummy2 D2;
+  std::auto_ptr<perftools::BaseServiceMaker> m(new perftools::ServiceMaker<D2>);
   
-  {
-    perftools::Sampler<int>  s(&more,&tell);
-    
-    CPPUNIT_ASSERT(s.sample()==1);
-    
-    
-    CPPUNIT_ASSERT(s.sample()==2);
-
-    CPPUNIT_ASSERT(last==0);
-
-  }    
-  
-  CPPUNIT_ASSERT(last==3);
-
-  {
-    perftools::Sampler<int>  s(&more,&tell);
-    CPPUNIT_ASSERT(last==3);
+  boost::any a = (*m)();
+  CPPUNIT_ASSERT(!a.empty());
+  try {
+    boost::shared_ptr<D2> d2 = boost::any_cast<boost::shared_ptr<D2> >(a);
+    CPPUNIT_ASSERT(d2);
+  } catch (boost::bad_any_cast const &) {
+    bool badAnyCast = false;
+    CPPUNIT_ASSERT(badAnyCast);
   }
 
-  CPPUNIT_ASSERT(last==1);
+}
 
+void TestServiceFactory::check_getAny() {
+
+}
+void TestServiceFactory::check_getService() {
+
+}
+
+void TestServiceFactory::check_NameError(){
+}
+
+void TestServiceFactory::check_TypeError() {
 }
