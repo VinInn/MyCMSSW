bdfb70a39e48cce271d8b310aef8fbba04f43c36
innocent <>
2007-01-29 06:58:05 +0000

    two plugins tests

--- PerfTools/Measure/test/plugins.cppunit.cc
+++ PerfTools/Measure/test/plugins.cppunit.cc
@@ -27,14 +27,12 @@ void TestPlugins::check_CPUClock() {
   typedef boost::function<long long(void)> Clock;
   boost::shared_ptr<Clock> c = perftools::ServiceFactory::get()->getService<Clock>("PerfTools:CPUClock");
   CPPUNIT_ASSERT(c);
-  Clock r(std::clock);
-  CPPUNIT_ASSERT((*c)==r);
+  CPPUNIT_ASSERT((*c)==&std::clock);
 }
 
 void TestPlugins::check_WallClock() {
   typedef boost::function<long long(void)> Clock;
   boost::shared_ptr<Clock> c = perftools::ServiceFactory::get()->getService<Clock>("PerfTools:WallClock");
   CPPUNIT_ASSERT(c);
-  Clock r(perftools::realTime);
-  CPPUNIT_ASSERT((*c)==r);
+  CPPUNIT_ASSERT((*c)==&perftools::realTime);
 }
