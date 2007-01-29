da5e9f951fa86248363f5055966351843e2cd408
innocent <>
2007-01-29 06:48:30 +0000

    two plugins tests

---
+++ PerfTools/Measure/test/plugins.cppunit.cc
@@ -0,0 +1,38 @@
+#include "PerfTools/Service/interface/ServiceFactory.h"
+#include<memory>
+#include<typeinfo>
+#include "PerfTools/Measure/interface/RealTime.h"
+#include <boost/function.hpp>
+#include <ctime>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+class TestPlugins : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(TestPlugins);
+  CPPUNIT_TEST(check_CPUClock);
+  CPPUNIT_TEST(check_WallClock);
+  CPPUNIT_TEST_SUITE_END();
+public:
+  void setUp(){}
+  void tearDown() {}
+  void check_CPUClock();  
+  void check_WallClock();
+};
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestPlugins);
+
+
+void TestPlugins::CPUClock() {
+  typedef boost::function<long long(void)> Clock;
+  boost::shared_ptr<Clock> c = perftools::ServiceFactory::get()->getService<Clock>("PerfTools:CPUClock");
+  CPPUNIT_ASSERT(c);
+  CPPUNIT_ASSERT(*c==Clock(std::clock));
+}
+
+void TestPlugins::WallClock() {
+  typedef boost::function<long long(void)> Clock;
+  boost::shared_ptr<Clock> c = perftools::ServiceFactory::get()->getService<Clock>("PerfTools:WallClock");
+  CPPUNIT_ASSERT(c);
+  CPPUNIT_ASSERT(*c==Clock(perftools::realTime));
+}
