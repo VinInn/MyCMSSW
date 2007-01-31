a15e478ac687dc6a3c58ab788cde9b26cde84351
innocent <>
2007-01-31 13:47:58 +0000

    builder

--- PerfTools/Reporter/src/SamplerBuilder.cc
+++ PerfTools/Reporter/src/SamplerBuilder.cc
@@ -22,26 +22,27 @@ namespace perftools {
     if (name.find("Clock")!=std::string::npos) {
       boost::shared_ptr<Clock> c = perftools::ServiceFactory::get()->getService<Clock>("PerfTools:"+name);
       return perftools::SamplerImpl<long long>(*c,
-					       boost::bind(&R::operator()<long long>,
+					       boost::bind(&R::template operator()<long long>,
 							   reporter,name.c_str(),_1),false,true);
     }
     else{}
   }
   
-
+  
   SamplerBuilder::SamplerBuilder(){}
-
+  
   SamplerBuilder::Payload & 
   SamplerBuilder::operator()(std::vector<std::string> const & sources,
-			     std::vector<std::string> const &) {
-
-    if (sources.empty()||reporter.empty()) return m_payload;
+			     std::vector<std::string> const & reporters) {
+    
+    if (sources.empty()||reporters.empty()) return m_payload;
     // full combinatorics...
     //FIXME now just one reporter
     m_payload.resize(sources.size());
-    boost::shared_ptr<perftools::SimpleImmediateReporter> sir(new perftools::SimpleImmediateReporter(std::cout,3));
-
-    std::transform(sources.begin(),source.end(),
+    // FIXME here goes a factory..
+    boost::shared_ptr<perftools::SimpleImmediateReporter> sir(new perftools::SimpleImmediateReporter(std::cout,sources.size()));
+    
+    std::transform(sources.begin(),sources.end(),
 		   m_payload.begin(),
 		   boost::bind(load< boost::shared_ptr<perftools::SimpleImmediateReporter> >,_1,sir));
     return m_payload;
