9063e7b22c6e4327c0fe20c7a4658296db0dbad7
innocent <>
2007-01-31 14:14:36 +0000

    builder test

---
+++ PerfTools/Reporter/test/SamplerBuilder.cppunit.cc
@@ -0,0 +1,64 @@
+#include "PerfTools/Reporter/interface/SamplerBuilder.h"
+#include "PerfTools/Reporter/interface/Sample.h"
+#include "PerfTools/Reporter/interface/Sampler.h"
+
+
+
+#include <cmath>
+#include <typeinfo> 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <boost/assign/std/vector.hpp>
+#include <boost/assign/list_of.hpp>
+using namespace boost::assign;
+
+
+#include <cppunit/extensions/HelperMacros.h>
+
+
+
+namespace {
+
+  double gcrap=0;
+  void waiste() {
+    for (double i=1;i<100000;i++)
+      gcrap+=std::log(std::sqrt(i));
+  }
+}
+
+// FIXME
+// I would have preferred check by features....
+class TestSamplerBuilder : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(TestSamplerBuilder);
+  CPPUNIT_TEST(check_Timers);
+  CPPUNIT_TEST_SUITE_END();
+public:
+  void setUp(){}
+  void tearDown() {}
+  void check_Timers();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestSamplerBuilder);
+
+
+//FIXME this is an integration test!
+void TestSamplerBuilder::check_Timers() {
+  perftools::SamplerBuilder builder;
+  std::vector<std::string> sources;
+  sources += "Wall", "CPU";
+  std::vector<std::string> reporters;
+  perftools::Sample s1(builder(sources,reporters));
+
+  {	   
+    perftools::Sampler(s1);
+    waiste();
+  }
+  {	   
+    perftools::Sampler(s1);
+    waiste();
+  }
+
+}
+
+
