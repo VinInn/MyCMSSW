4161f5f45bab823d23bd74beddfb4da718349879
innocent <>
2007-02-16 14:37:18 +0000

    UsedMemory

--- PerfTools/Measure/test/plugins.cppunit.cc
+++ PerfTools/Measure/test/plugins.cppunit.cc
@@ -11,12 +11,14 @@ class TestPlugins : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(TestPlugins);
   CPPUNIT_TEST(check_CPUClock);
   CPPUNIT_TEST(check_WallClock);
+  CPPUNIT_TEST(check_UsedMemory);
   CPPUNIT_TEST_SUITE_END();
 public:
   void setUp(){}
   void tearDown() {}
   void check_CPUClock();  
   void check_WallClock();
+}  void check_UsedMemory();
 };
 
 
@@ -28,6 +30,13 @@ void TestPlugins::check_CPUClock() {
   boost::shared_ptr<Clock> c = perftools::ServiceFactory::get()->getService<Clock>("PerfTools:CPUClock");
   CPPUNIT_ASSERT(c);
   CPPUNIT_ASSERT((*c)==&std::clock);
+  try {
+    (*c)();
+  }
+  catch(...) {
+    bool ErrorInInvokingCPUClock=false;
+    CPPUNIT_ASSERT(ErrorInInvokingCPUClock);
+  }
 }
 
 void TestPlugins::check_WallClock() {
@@ -35,4 +44,24 @@ void TestPlugins::check_WallClock() {
   boost::shared_ptr<Clock> c = perftools::ServiceFactory::get()->getService<Clock>("PerfTools:WallClock");
   CPPUNIT_ASSERT(c);
   CPPUNIT_ASSERT((*c)==&perftools::realTime);
+  try {
+    (*c)();
+  }
+  catch(...) {
+    bool ErrorInInvokingWallClock=false;
+    CPPUNIT_ASSERT(ErrorInInvokingCPUClock);
+  }
+}
+void TestPlugins::check_UsedMemory() {
+  typedef boost::function<int(void)> Mem;
+  boost::shared_ptr<Mem> c = perftools::ServiceFactory::get()->getService<Mem>("PerfTools:UsedMemory");
+  CPPUNIT_ASSERT(c);
+  try {
+    (*c)();
+  }
+  catch(...) {
+    bool ErrorInInvokingCPUClock=false;
+    CPPUNIT_ASSERT(ErrorInInvokingUsedMemory);
+  }
+
 }
