7b1144c9c374c3b547a11ea76a3619e642f57994
innocent <>
2007-04-28 07:15:47 +0000

    more tests

---
+++ PoolTests/SchemaEvolution/interface/SimpleTest.h
@@ -0,0 +1,27 @@
+#ifndef SchemaEvolution_SimpleTests_H
+#define SchemaEvolution_SimpleTests_H
+
+namespace shevsp {
+
+  struct B {
+    virtual ~B(){}
+    int i;
+  };
+
+  struct A1 : public B {};
+  struct A2 : public B {};
+
+  struct C {
+    C() : i(0),b(0);
+    typedef A1 M1;
+    typedef A2 M2;
+    int i;
+    M1 m1;
+    M2 m2;
+    B * b;
+
+  };
+  
+
+}
+#endif
