fd6e2d0a03262ca3a23024e8bb8d52a860a137c3
innocent <>
2007-04-28 07:50:39 +0000

    more tests

--- PoolTests/SchemaEvolution/test/verifyReflex.cpp
+++ PoolTests/SchemaEvolution/test/verifyReflex.cpp
@@ -19,6 +19,7 @@ void dump(ROOT::Reflex::Object & ob) {
     std::cout<< tc.TypeTypeAsString() << " "  
 	     << tc.Name(ROOT::Reflex::SCOPED) << " "
 	     << tc.RawType().Name(ROOT::Reflex::SCOPED) 
+	     << ob.DynamicType().Name(ROOT::Reflex::SCOPED) << " "
 	     << " at " <<  ob.Address() << std::endl;
     
     if (!(tc.IsClass()||tc.IsStruct())) return; 
@@ -31,7 +32,7 @@ void dump(ROOT::Reflex::Object & ob) {
     
     for (size_t i=0; i<tc.DataMemberSize(); i++) {
       Member m = tc.DataMemberAt(i);
-      std::cout << "member " << m.Name();
+      std::cout << "member " << m.Name() <<" ";
       ROOT::Reflex::Object lo = m.Get(ob); 
       dump(lo);
     }
@@ -41,7 +42,10 @@ void dump(ROOT::Reflex::Object & ob) {
 int main() {
 
   shevsp::C c;
+  std::cout << (void*)(&c) << std::endl; 
   c.b = new shevsp::A1();
+  std::cout << (void*)(c.b) << std::endl; 
+  std::cout << (void*)(&c.b) << std::endl; 
   ROOT::Reflex::Type type = ROOT::Reflex::Type::ByTypeInfo(typeid(c));
   ROOT::Reflex::Object ob(type,&c);
   dump(ob);
