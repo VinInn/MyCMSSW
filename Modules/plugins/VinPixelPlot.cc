7150d54459ff964ad2eafcf3fc33bdaccbc951ef
innocent <>
2012-09-14 07:48:34 +0000

    print it

--- Modules/plugins/VinPixelPlot.cc
+++ Modules/plugins/VinPixelPlot.cc
@@ -8,9 +8,8 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-//
-// class declaration
-//
+
+#include <DataFormats/SiPixelCluster/interface/SiPixelCluster.h>
 
 class VinPixelPlot : public edm::EDAnalyzer {
 public:
@@ -23,7 +22,15 @@ public:
 private:
   virtual void beginJob() {}
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
-  virtual void endJob() {}
+  virtual void endJob();
+
+  std::string pixelclusterLabel_;
+
+  long long nev=0;
+  long long nclus=0;
+  float maxRatio=0.f;
+  double totRatio=0.f;
+
 };
 
 //define this as a plug-in
@@ -33,9 +40,28 @@ DEFINE_FWK_MODULE(VinPixelPlot);
 void
 VinPixelPlot::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
 
-  //  edm::Handle<ExampleData> pIn;
-  // iEvent.getByLabel("example",pIn);
-
+  nev++;
+  edm::Handle<edmNew::DetSetVector<SiPixelCluster> > pixelclusters;
+  iEvent.getByLabel(pixelclusterLabel_,pixelclusters  );    
+  for (auto const &  DSV : *pixelclusters ) {
+    uint32_t detid = DSV.id();
+    nclus += DSV.size();
+    for(auto const & clus : DSV) {  
+      double n = clus.size();
+      double nx= clus.sizeX();
+      double ny= clus.sizeY();
+      double ratio = n/(nx*ny);
+      maxRatio = std::max(maxRatio,ratio);
+      totRatio+=ratio;
+    }
+  }
   std::cout << "ciao" << std::endl;
 
+}
+
+#include<cstdio>
+void VinPixelPlot::endJob() {
+
+  printf("\nVinPixelPlot: nev/nclus/maxRatio,aveRatio %d/%d/%f/%f\n",nev,nclus,maxRatio,totRatio/double(nclus));
+  
 }
