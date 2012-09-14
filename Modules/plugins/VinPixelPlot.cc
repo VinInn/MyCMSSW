884ebacba134ad7e08299c47dc892538d246a96c
innocent <>
2012-09-14 08:34:48 +0000

    print it

--- Modules/plugins/VinPixelPlot.cc
+++ Modules/plugins/VinPixelPlot.cc
@@ -9,7 +9,11 @@
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include <DataFormats/SiPixelCluster/interface/SiPixelCluster.h>
+#include "DataFormats/SiPixelCluster/interface/SiPixelCluster.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "TH1F.h"
 
 class VinPixelPlot : public edm::EDAnalyzer {
 public:
@@ -20,7 +24,7 @@ public:
   
   
 private:
-  virtual void beginJob() {}
+  virtual void beginJob();
   virtual void analyze(const edm::Event&, const edm::EventSetup&);
   virtual void endJob();
 
@@ -32,10 +36,28 @@ private:
   double minRatio=2.;
   double totRatio=0.;
 
+  TH1F * hClusSize;
+  TH1F * hClusSizeX;
+  TH1F * hClusSizeY;
+  TH1F * hClusSizeRatio;
+  TH1F * hClusSizeInvRatio;
+
 };
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(VinPixelPlot);
+
+void VinPixelPlot::beginJob() {
+
+  edm::Service<TFileService> fs;
+  
+  hClusSize=fs->make<TH1F>("clusSize", "cluster size",100,0.,1000.) ;
+  hClusSizeX=fs->make<TH1F>("clusSizeX", "cluster X size",100,0.,1000.) ;
+  hClusSizeY=fs->make<TH1F>("clusSizeY", "cluster Y size",100,0.,1000.) ;
+  hClusSizeRatio=fs->make<TH1F>("clusRatio", "size/(x*y)",100,0.,1.);
+  hClusSizeRatio=fs->make<TH1F>("clusInvRatio", "(x*y)/size",100,0.,10.);
+
+}
 
 #include <iostream>
 void
@@ -52,9 +74,15 @@ VinPixelPlot::analyze(const edm::Event& iEvent, const edm::EventSetup&) {
       double nx= clus.sizeX();
       double ny= clus.sizeY();
       double ratio = n/(nx*ny);
-      maxRatio = std::max(maxRatio,1./ratio);
-      minRatio = std::max(minRatio,ratio);
+      maxRatio = std::max(maxRatio,ratio);
+      minRatio = std::max(minRatio,1./ratio);
       totRatio+=ratio;
+      hClusSize->Fill(n);
+      hClusSizeX->Fill(nx);
+      hClusSizeY->Fill(ny);
+      hClusSizeRatio->Fill(ratio);
+      hClusSizeInvRatio->Fill(1./ratio);
+
     }
   }
   // std::cout << "ciao" << std::endl;
--- Modules/tests/runit.py
+++ Modules/tests/runit.py
@@ -7,11 +7,13 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
         'file:reco2012.root'
     )
 )
+
+process.TFileService = cms.Service("TFileService",fileName=cms.string('vHisto.root'))
+
 
 process.demo = cms.EDAnalyzer('VinPixelPlot')
 process.mypath = cms.Path(process.demo)
